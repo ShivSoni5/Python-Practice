999d6daf55c24ef67867faf6b4a7adcde07dc936
ShivSoni5 <shivsonic05@gmail.com>
2017-10-29 04:03:28 +0530

    bg_clr = black and created own module 'Shiv'

--- pygame/first_game.py
+++ pygame/first_game.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
 import pygame,random
+from Shiv import shiv
+
+
 
 STARTING_BLUE_BLOBS = 10
 STARTING_RED_BLOBS=13
 
 WIDTH = 800
 HEIGHT = 600
-WHITE = (255,255,255)
+BLACK = (0,0,0)
 BLUE = (0,0,255)
 RED = (255,0,0)
 
@@ -16,44 +19,20 @@ pygame.display.set_caption("shiv world")
 clock = pygame.time.Clock()
 
 
-class shiv:
-
-	def __init__(self,color):
-        	self.x = random.randrange(0,WIDTH)
-        	self.y = random.randrange(0, HEIGHT)
-        	self.size = random.randrange(2,9)
-        	self.color = color
-
-	def move(self):
-        	self.move_x = random.randrange(-1,2)
-        	self.move_y = random.randrange(-1,2)
-        	self.x += self.move_x
-		self.y += self.move_y
-
-		if self.x < 0 :
-			self.x=0
-		elif self.x > WIDTH :
-			self.x = WIDTH
-
-		if self.y < 0:
-			self.y = 0
-		elif self.y > HEIGHT:
-			self.y = HEIGHT
-
-
 def draw_environment(blob_list):
-	game_display.fill(WHITE)
+	game_display.fill(BLACK)
 
 	for blob_dict in blob_list:
 		for blob_id in blob_dict:
 			blob = blob_dict[blob_id]
 			pygame.draw.circle(game_display, blob.color, [blob.x, blob.y], blob.size)
 			blob.move()
+			blob.check_bounds()
 	pygame.display.update()
 
 def main():
-	blue_blobs = dict(enumerate([shiv(BLUE) for i in range(STARTING_BLUE_BLOBS)]))
-	red_blobs = dict(enumerate([shiv(RED) for i in range(STARTING_RED_BLOBS)]))
+	blue_blobs = dict(enumerate([shiv(BLUE, WIDTH, HEIGHT) for i in range(STARTING_BLUE_BLOBS)]))
+	red_blobs = dict(enumerate([shiv(RED, WIDTH, HEIGHT) for i in range(STARTING_RED_BLOBS)]))
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
