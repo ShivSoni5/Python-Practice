b0989e87d86ab9f8ea7e724c42b83693e6b14600
ShivSoni5 <shivsonic05@gmail.com>
2017-10-30 00:39:27 +0530

    first and last decorator

---
+++ Decorators.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+from functools import wraps
+
+def add_wrapping(item):
+	@wraps(item)
+	def wrapped_item():
+		return 'a wrapped up box of {}'.format(str(item))
+	return wrapped_item	
+
+"""
+def add_wrapping1(item):
+	def wrapped_item():
+		return 'a wrapped up box of {}'.format(str(item()))
+	return wrapped_item
+"""
+@add_wrapping
+def new_laptop():
+	return "dell xps 1300"
+
+print(new_laptop())
+print(new_laptop.__name__)
+"""
+@add_wrapping1    
+def new_200ns():
+	return "laptop on my new bike pulsar 200ns"
+
+
+print(new_200ns())
+print(new_200ns.__name__)
+"""
