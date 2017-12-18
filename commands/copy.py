bf944ba270b8138daed817ab2b02c42c68c9d43f
ShivSoni5 <shivsonic05@gmail.com>
2017-12-18 16:50:13 +0530

    final command

--- commands/copy.py
+++ commands/copy.py
@@ -1,22 +1,37 @@
-#usr/bin/python
+#!/usr/bin/python
 
 import os,sys
 
-s=raw_input("Enter the directory of file to be copied: ")
+#s=raw_input("Enter the directory of file to be copied: ")
+#os.chdir(s)
 
-os.chdir(s)
+s=sys.argv[1]
+r=sys.argv[2]
 
-r=raw_input("enter file: ")
-f=open(r,"r")
+#r=raw_input("enter file: ")
+#f=open(r,"r")
+#d=raw_input("Enter the destination directory of file be copied: ")
+#os.chdir(d)
 
-x = f.read()
+def copy(a,b):
+	
+	f=open(a,"r")
+	x = f.read()
+	f.close()
 
-f.close()
-d=raw_input("Enter the destination directory of file be copied: ")
+	try:
+	
+		f1= open(b,"w")
+		f1.write(x)
+		f1.close()
+	
+	except IOError:        #when name is not provided eg.  cp /root/Desktop/shiv.py /root/Document/
+		os.chdir(b)
+		c=a.split('/')
+		f1=open(c[-1],"w")
+		f1.write(x)
+		f1.close()
 
-os.chdir(d)
-
-f1= open(r,"w")
-f1.write(x)
-
-f1.close()
+copy(s,r)
+		
+	
