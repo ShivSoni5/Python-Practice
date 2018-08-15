bc7b1e856a3d29415c1e148dac82c3655b6b3e0e
ShivSoni5 <shivsonic05@gmail.com>
2018-08-15 10:20:36 +0530

    insert many elements

---
+++ connect_Mongo/insert_many.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from pymongo import MongoClient
+client = MongoClient()
+
+db = client.pymongo_new
+
+posts = db.posts
+post_1 = {
+    'title': 'Python and MongoDB',
+    'content': 'PyMongo is fun, you guys',
+    'author': 'Styris'
+}
+post_2 = {
+    'title': 'Virtual Environments',
+    'content': 'Use virtual environments, you guys',
+    'author': 'William'
+}
+post_3 = {
+    'title': 'Learning Python',
+    'content': 'Learn Python, it is easy',
+    'author': 'Bill'
+}
+
+result = posts.insert_many([post_1,post_2,post_3])
+print(f'Mulitple Posts: {result.inserted_ids}')
