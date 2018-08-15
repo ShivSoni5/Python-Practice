ec15bb6a716d18e8166c4b1ef068c6d4858f3a01
ShivSoni5 <shivsonic05@gmail.com>
2018-08-15 17:59:27 +0530

    learn bottle

---
+++ connect_Mongo/NewBottle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from bottle import route, run, template
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+run(host='localhost', port=8080)
+
--- connect_Mongo/insert_Documents.py
+++ connect_Mongo/insert_Documents.py
@@ -11,7 +11,7 @@ db = client.pymongo_new
 # db = client['pymongo_test']  # Same as above
 
 # Inserting Documents
-
+'''
 posts = db.posts
 post_data = {
         'title': 'What is Python?',
@@ -19,4 +19,11 @@ post_data = {
         'author': 'None'
         }
 result = posts.insert_one(post_data)
+'''
+
+name = db.names
+n = {'name':'shiv',
+        'gender':'boy'}
+
+result = name.insert_one(n)
 print(f'One post: {result.inserted_id}')
