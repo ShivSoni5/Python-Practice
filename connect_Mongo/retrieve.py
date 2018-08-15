bbfd46fcc2676e067a96413097a6fed97d445621
ShivSoni5 <shivsonic05@gmail.com>
2018-08-15 14:34:14 +0530

    retrieve every post

--- connect_Mongo/retrieve.py
+++ connect_Mongo/retrieve.py
@@ -6,5 +6,12 @@ client = MongoClient()
 db = client.pymongo_new
 
 posts = db.posts
-william_post = posts.find_one({'author': 'William'})
-print(william_post)
+# william_post = posts.find_many({'author': 'William'})
+william_post = posts.find({'author': 'William'})
+for i in william_post:
+    print(i)
+print()
+
+every_post = posts.find()
+for i in every_post:
+    print(i)
